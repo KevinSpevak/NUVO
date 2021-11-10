3cf71e09e41d7cbc17da04715bf82d7cd0cb068f
Kevin Spevak <kevinspevak@gmail.com>
2021-11-09 18:51:39 -0700

    Add test binocular video

--- binocular_video_capture.py
+++ binocular_video_capture.py
@@ -8,7 +8,7 @@ import glob
 def main():
     # Define constants
     # Read images from the left camera
-    video_capture_left = cv2.VideoCapture(1)  # Open video capture object
+    video_capture_left = cv2.VideoCapture(6)  # Open video capture object
 
     # Read images from the right camera
     video_capture_right = cv2.VideoCapture(2)  # Open video capture object
@@ -16,25 +16,28 @@ def main():
     got_image_left, bgr_img_left = video_capture_left.read()  # Make sure we can read video from the left and right
     got_image_right, bgr_img_right = video_capture_right.read()
 
-    if not got_image_left or got_image_right:
+    if not got_image_left or not got_image_right:
         print("Cannot read one of the video sources")
         sys.exit()
 
     fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
 
-    videoWriter_left = cv2.VideoWriter("binocular_test_left.avi", fourcc=fourcc, fps=24.0,
+    videoWriter_left = cv2.VideoWriter("data/original/binocular_test_left.avi", fourcc=fourcc, fps=24.0,
                                   frameSize=(bgr_img_left.shape[1], bgr_img_left.shape[0]))
-    videoWriter_right = cv2.VideoWriter("binocular_test_right.avi", fourcc=fourcc, fps=24.0,
+    videoWriter_right = cv2.VideoWriter("data/original/binocular_test_right.avi", fourcc=fourcc, fps=24.0,
                                        frameSize=(bgr_img_right.shape[1], bgr_img_right.shape[0]))
 
-    while True:
+    for x in range(int(720)):
         got_image_left, bgr_img_left = video_capture_left.read()
         got_image_right, bgr_img_right = video_capture_right.read()
 
-        if not got_image_left or got_image_right:
+        if not got_image_left or not got_image_right:
             print("Breaking out :/")
             break  # End of video; exit the while loop
 
+        cv2.imshow("left", bgr_img_left)
+        cv2.imshow("right", bgr_img_right)
+        cv2.waitKey(40)
         videoWriter_left.write(bgr_img_left)
         videoWriter_right.write(bgr_img_right)
     videoWriter_left.release()
