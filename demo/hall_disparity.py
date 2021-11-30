Corrected depth computation by dividing disparity map by 16

demo/hall_disparity.py
@@ -1,6 +1,7 @@
 import pickle
 import cv2
 import numpy as np
+from utils.computeAvgForegroundDepth import *
 
 def main():
     cam_left = pickle.load(open("data/calibration/intrinsics_left.p", "rb"))
@@ -19,8 +20,8 @@ def main():
                                    sgbm_params["P1"], sgbm_params["P2"], sgbm_params["disp12MaxDiff"], sgbm_params["preFilterCap"],
                                    sgbm_params["uniquenessRatio"], sgbm_params["speckleWindowSize"], sgbm_params["speckleRange"])
 
-    video_left = cv2.VideoCapture("data/original/hall_left.avi")
-    video_right = cv2.VideoCapture("data/original/hall_right.avi")
+    video_left = cv2.VideoCapture("data/original/wall_left.avi")
+    video_right = cv2.VideoCapture("data/original/wall_right.avi")
     while True:
         got_left, img_left = video_left.read()
         got_right, img_right = video_right.read()
@@ -30,9 +31,16 @@ def main():
         img_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
         rectified_left = cv2.remap(img_left, map_left_1, map_left_2, cv2.INTER_LINEAR)
         rectified_right = cv2.remap(img_right, map_right_1, map_right_2, cv2.INTER_LINEAR)
-        disparity = stereo.compute(rectified_left, rectified_right).astype(np.float32)
+
+        # Magic number 16 used here - collective comprehension for the reason is low
+        disparity = stereo.compute(rectified_left, rectified_right).astype(np.float32)/16
         max, min = disparity.max(), disparity.min()
         cv2.imshow("disparity", (disparity - min)/(max-min))
+
+        image_3d = cv2.reprojectImageTo3D(disparity, Q)
+        img_left = computeAvgForegroundDepth(rectified_left, image_3d)
+        cv2.imshow('Calculating depth at box', img_left)
+        cv2.waitKey(30)
 
         if cv2.waitKey(40) == 27:
             break

utils/computeAvgForegroundDepth.py
@@ -4,25 +4,34 @@ import cv2
 def computeAvgForegroundDepth(bgr_img, image_3d):
     img_height = bgr_img.shape[0]
     img_width = bgr_img.shape[1]
-    x = int(img_width / 2)
-    y = int(img_height / 2) - 10
+    x = int(img_width / 2) -15
+    y = int(img_height / 2) -15
     w = 30
     h = 30
-    depth_list = []
 
     cv2.rectangle(bgr_img, (x, y), (x + w, y + h), (255, 0, 255), 2)
     cv2.imshow('left', bgr_img)
     cv2.waitKey(30)
 
     depth_sum = 0
+    x_sum = 0
+    y_sum = 0
     num_pixels = 0
+
     for i in range(w):
         for j in range(h):
             if image_3d[x + i][y + j][2] != np.inf:
                 num_pixels += 1
+                x_sum += image_3d[x + i][y + j][0]
+                y_sum += image_3d[x + i][y + j][1]
                 depth_sum += image_3d[x + i][y + j][2]
-                # print('Local depth at ',(x+i,y+j),' : ', image_3d[x+i][y+j][2])
+
     avg_depth = depth_sum / num_pixels
+    avg_x = x_sum/num_pixels
+    avg_y = y_sum / num_pixels
+    # print("Num reasonable pixels: ", num_pixels)
+    # print("x,y,z: ", (avg_x,avg_y,avg_depth))
+
     # depth_list = np.append(depth_list, avg_depth)
     # scipy.io.savemat('depth_list.mat', {'depth_list': depth_list})
     print("Average depth at square is: ", avg_depth)